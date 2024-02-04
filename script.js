2e81c8522dcc7e59643dbff18f654c3b8e6df098
Yordii01 <48106292+Yordii01@users.noreply.github.com>
2024-02-04 10:46:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "TAAK <3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -42,12 +42,12 @@ function resizeYesButton() {
 
 function generateMessage(noCount) {
   const messages = [
-    "No",
-    "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
+    "NIE",
+    "NA PEWNO?",
+    "ZLITUJ SIE",
+    "NIE RÓB MI TEGO:(",
+    "NO PROSZE",
+    "BEDE PŁAKAŁ",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
